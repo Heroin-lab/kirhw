2743a7c4a3c576c46135675fe4d9e72e018c5406
Heroin-lab <vip.serkin@gmail.com>
2021-04-01 12:51:16 +0300

    half update

---
+++ 6hw/script.js
@@ -0,0 +1,32 @@
+// 1 task \\
+
+// function getCookingTime (eggsAmount) {
+//     var result;
+//     result = Math.ceil(eggsAmount / 5) * 5;
+//     return result;
+// }    
+
+// console.log(getCookingTime(11));
+
+// 2 task \\
+
+// function getNumber (array) {
+//     var result;
+//     var odd = [];
+//     var even = [];
+//     for (var i = 0; i < array.length; i++) {
+//         if (array[i] % 2 === 0) {
+//             even.push(array[i]);
+//         } else {
+//             odd.push(array[i]);
+//         }
+//     }
+//     odd.length > even.length ? result = even[0] : result = odd[0];
+//     return result;
+// }
+
+// console.log(getNumber([1, 5, 7, 9, 15, 19, 777, -15, -11, 4, 9, 23, -17]));
+// console.log(getNumber([0, 2, 8, -4, 0, -122, 13, -4, 28, 12]));
+
+// 3 task \\
+
